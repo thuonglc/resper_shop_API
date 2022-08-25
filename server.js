a63598156c201ca0080c2ce2bbd231ccd72cb150
resper <thuongresper@gmail.com>
2022-08-25 23:09:20 +0700

    v1

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-app.listen(process.env.PORT || 5000, function () {
-	console.log(
-		'Express server listening on port %d in %s mode',
-		this.address().port,
-		app.settings.env
-	);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`WSV start with port ${PORT}`);
 });
