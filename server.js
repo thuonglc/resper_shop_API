215547ac15e5ba21d30d25660629290aeb04c967
resper <thuongresper@gmail.com>
2022-08-25 21:57:31 +0700

    v1: fix port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`WSV start with port ${PORT}`);
